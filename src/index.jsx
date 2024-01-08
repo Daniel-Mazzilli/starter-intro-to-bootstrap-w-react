bac8fea553ee2be317e664c1d4369e7946cfb24a
Daniel-Mazzilli <danmazzilli@pursuit.org>
2024-01-08 14:59:40 -0500

    class code, added nav

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-import App from "./App";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
